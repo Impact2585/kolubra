180361e22b021a58dc2d9cd41071a5390e660270
jxin31415 <jxin31415@gmail.com>
2021-03-28 00:05:35 -0500

    Basic functionality confirmation

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,14 @@ import 'Creature.dart';
 
 List<Creature> allCreatures = List();
 
-void init() { //such a hassle is there a way to automate this
+void init() {
+  //such a hassle is there a way to automate this
   Creature Jimmy = new Creature("Jimmy");
   Jimmy.addStage("assets/JimmyAssets/jimmy1.png", 0);
   Jimmy.addStage("assets/JimmyAssets/jimmy2.png", 50);
   allCreatures.add(Jimmy);
 }
+
 void main() {
   init();
   runApp(Kolubra());
@@ -27,8 +29,8 @@ class Kolubra extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeLeft,
-      DeviceOrientation.landscapeRight,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
     ]);
     return MaterialApp(
       title: 'Kolubra',
@@ -49,40 +51,6 @@ class Kolubra extends StatelessWidget {
   }
 }
 
-class FitKitReader {
-  static String result = "";
-  static Map<DataType, List<FitData>> results = Map();
-  static bool permissions;
-  static Future<void> read() async {
-    results.clear();
-
-    try {
-      permissions = await FitKit.requestPermissions(DataType.values);
-      if (!permissions) {
-        result = 'requestPermissions: failed';
-      } else {
-        for (DataType type in DataType.values) {
-          try {
-            results[type] = await FitKit.read(
-              type,
-              dateFrom: DateTime.now().subtract(Duration(days: 5)),
-              dateTo: DateTime.now(),
-            );
-          } on UnsupportedException catch (e) {
-            print("you fool");
-            print(e);
-            results[e.dataType] = [];
-          }
-        }
-
-        result = 'readAll: success';
-      }
-    } catch (e) {
-      result = 'readAll: $e';
-    }
-    print(result);
-  }
-}
 class Home extends StatefulWidget {
   Home({Key key, this.title}) : super(key: key);
 
@@ -101,62 +69,154 @@ class Home extends StatefulWidget {
   HomeState createState() => HomeState();
 }
 
-
 class HomeState extends State<Home> {
   String currentForm = allCreatures[0].getImageByEnergy();
+  String result = "";
+  Map<DataType, List<FitData>> results = Map();
+  bool permissions;
+
+  @override
+  void initState() {
+    super.initState();
+    hasPermissions();
+  }
+
+  Future<void> read() async {
+    results.clear();
+
+    try {
+      permissions = await FitKit.requestPermissions(DataType.values);
+      if (!permissions) {
+        result = 'requestPermissions: failed';
+      } else {
+        for (DataType type in DataType.values) {
+          try {
+            results[type] = await FitKit.read(
+              type,
+              dateFrom: DateTime.now().subtract(Duration(days: 5)),
+              dateTo: DateTime.now(),
+            );
+          } on UnsupportedException catch (e) {
+            results[e.dataType] = [];
+          }
+        }
+
+        result = 'readAll: success';
+      }
+    } catch (e) {
+      result = 'readAll: $e';
+    }
+
+    setState(() {});
+  }
+
+  Future<void> revokePermissions() async {
+    results.clear();
+
+    try {
+      await FitKit.revokePermissions();
+      permissions = await FitKit.hasPermissions(DataType.values);
+      result = 'revokePermissions: success';
+    } catch (e) {
+      result = 'revokePermissions: $e';
+    }
+
+    setState(() {});
+  }
+
+  Future<void> hasPermissions() async {
+    try {
+      permissions = await FitKit.hasPermissions(DataType.values);
+    } catch (e) {
+      result = 'hasPermissions: $e';
+    }
+
+    if (!mounted) return;
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/clubpenguinmap.png'),
-          fit: BoxFit.fill,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/clubpenguinmap.png'),
+            fit: BoxFit.fill,
+          ),
         ),
-      ),
         child: Stack(
           children: <Widget>[
             Align(
               alignment: Alignment.bottomLeft,
-              child: FloatingActionButton(
-                heroTag: null,
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) => _buildPopupDialog(context),
-                  );
-                },
-                // child: HexagonWidget.flat(
-                //   width: 5,
-                //   color: Colors.limeAccent,
-                //   padding: 4.0,
-                //   child: Icon(exclamationmark, color: Colors.black),
-                // ),
-                child: Icon(CupertinoIcons.exclamationmark,
-                    color: Colors.black, size: 25),
-                backgroundColor: Colors.limeAccent[400]
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: FloatingActionButton(
+                  heroTag: null,
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          _buildQuestDialog(context),
+                    );
+                  },
+                  // child: HexagonWidget.flat(
+                  //   width: 5,
+                  //   color: Colors.limeAccent,
+                  //   padding: 4.0,
+                  //   child: Icon(exclamationmark, color: Colors.black),
+                  // ),
+                  child: Icon(CupertinoIcons.exclamationmark,
+                      color: Colors.black, size: 25),
+                  backgroundColor: Colors.limeAccent[400],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                ),
               ),
             ),
             Align(
               alignment: Alignment.bottomRight,
-              child: FloatingActionButton(
-                heroTag: null,
-                onPressed: () {
-                  FitKitReader.read();
-                  print(FitKitReader.result);
-                },
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: FloatingActionButton(
+                  heroTag: null,
+                  onPressed: () {
+                    read();
+                    print(result);
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          _buildSyncDialog(context),
+                    );
+                  },
+                  child: Icon(CupertinoIcons.refresh,
+                      color: Colors.black, size: 25),
+                  backgroundColor: Colors.green[600],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                ),
               ),
             ),
             Align(
               alignment: Alignment.bottomCenter,
-              child: FloatingActionButton(
-                heroTag: null,
-                onPressed: () {
-                  allCreatures[0].getEnergy(5);
-                  print(allCreatures[0].energy);
-                  currentForm = allCreatures[0].getImageByEnergy();
-                  print(currentForm);
-                  setState(() {}); // is there a less dumb way to do make sure it refreshes??
-                },
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: FloatingActionButton(
+                  heroTag: null,
+                  onPressed: () {
+                    allCreatures[0].getEnergy(5);
+                    print(allCreatures[0].energy);
+                    currentForm = allCreatures[0].getImageByEnergy();
+                    print(currentForm);
+                    setState(
+                        () {}); // is there a less dumb way to do make sure it refreshes??
+                  },
+                  child:
+                      Icon(CupertinoIcons.paw, color: Colors.black, size: 25),
+                  backgroundColor: Colors.blue[400],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                ),
               ),
             ),
             Container(
@@ -164,11 +224,107 @@ class HomeState extends State<Home> {
               child: Image.asset(currentForm, width: 100),
             )
           ],
-        )
-      );
+        ));
   }
 
-  Widget _buildPopupDialog(BuildContext context) {
+  Widget _buildSyncDialog(BuildContext context) {
+    List items =
+        results.entries.expand((entry) => [entry.key, ...entry.value]).toList();
+    print("ITEMS: ");
+    print(items);
+    return StatefulBuilder(builder: (context, setState) {
+      return AlertDialog(
+        content: Stack(
+          clipBehavior: Clip.none,
+          children: <Widget>[
+            Positioned(
+              right: -40.0,
+              top: -35.0,
+              child: InkResponse(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: CircleAvatar(
+                  child: Icon(Icons.close),
+                  backgroundColor: Colors.red,
+                  radius: 15,
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Row(
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          text: 'Sync to Google Fit',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        textScaleFactor: 1.5,
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          print("DF");
+                          setState(() {
+                            items = results.entries
+                                .expand((entry) => [entry.key, ...entry.value])
+                                .toList();
+                          });
+                        },
+                        child: Icon(CupertinoIcons.refresh,
+                            color: Colors.black, size: 25),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green, // background
+                          onPrimary: Colors.green, // foreground
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 600,
+                  width: 500,
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      final item = items[index];
+                      if (item is DataType) {
+                        return Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8),
+                          child: Text(
+                            '$item - ${results[item].length}',
+                            style: Theme.of(context).textTheme.title,
+                          ),
+                        );
+                      } else if (item is FitData) {
+                        return Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 4,
+                            horizontal: 8,
+                          ),
+                          child: Text(
+                            '$item',
+                            style: Theme.of(context).textTheme.caption,
+                          ),
+                        );
+                      }
+
+                      return Container();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
+  Widget _buildQuestDialog(BuildContext context) {
     return AlertDialog(
       content: Stack(
         clipBehavior: Clip.none,
@@ -195,10 +351,10 @@ class HomeState extends State<Home> {
                   padding: EdgeInsets.all(10),
                   child: RichText(
                     text: TextSpan(
-                      text: 'Challenges',
+                      text: 'Quests',
                       style: TextStyle(color: Colors.black),
                     ),
-                    textScaleFactor: 2,
+                    textScaleFactor: 1.5,
                   )),
               SizedBox(width: 10),
               Container(
@@ -206,10 +362,10 @@ class HomeState extends State<Home> {
                 padding: EdgeInsets.all(10),
                 child: RichText(
                   text: TextSpan(
-                    text: 'Lifetime Progression',
+                    text: 'Progression',
                     style: TextStyle(color: Colors.black),
                   ),
-                  textScaleFactor: 2,
+                  textScaleFactor: 1.5,
                 ),
               ),
             ],
